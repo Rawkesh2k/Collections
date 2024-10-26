97e4592657a426bfa4e4c1ab2f232445d251a214
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-10-26 18:56:22 -0400

    adding updated ArrayList

--- src/main/java/com/collections/java/List/ArrayListDemo.java
+++ src/main/java/com/collections/java/List/ArrayListDemo.java
@@ -1,4 +1,25 @@
 package com.collections.java.List;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        //Declaration
+
+        //This is a generic declaration, meaning, any type of data can be added/removed.
+        //be it integers, strings, booleans etc
+        ArrayList list = new ArrayList();
+
+        //adding elements
+        list.add(1);
+        list.add("sports");
+        list.add(true);
+        list.add('c');
+
+        //prints as an array
+        System.out.println(list);
+    }
+
+
 }
