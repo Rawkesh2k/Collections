c51d332d2031c882c8bdc66d2f271d55b1e58359
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-11-11 00:28:49 -0500

    adding updated EnumSet

--- src/main/java/com/collections/java/set/EnumSetImpl.java
+++ src/main/java/com/collections/java/set/EnumSetImpl.java
@@ -28,10 +28,24 @@ public class EnumSetImpl {
         EnumSet<Lang> range = EnumSet.range(Lang.Java, Lang.Python);
         System.out.println(range);
 
-        //of()
+        //of() - to create an enumset of specific element
         EnumSet<Lang> cSharpEnum = EnumSet.of(Lang.CSharp);
         System.out.println(cSharpEnum);
 
+        //to create an enumset of multiple elements
+        EnumSet<Lang> multiENumVals = EnumSet.of(Lang.Linux, Lang.Java);
+        System.out.println(multiENumVals);
 
+        //add(), addAll()
+
+        EnumSet<Lang> lang1 = EnumSet.allOf(Lang.class);
+        EnumSet<Lang> lang2 = EnumSet.noneOf(Lang.class);
+
+        //adding one element from lang1 to empty lang2
+        lang2.add(Lang.Linux);
+        System.out.println(lang2);
+        //adding all elements from lang1 to lang2
+        lang2.addAll(lang1);
+        System.out.println(lang2);
     }
 }
