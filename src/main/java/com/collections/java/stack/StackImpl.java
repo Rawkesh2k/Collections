26e9ec7ff21f1b358f10e6cda31eb31d61a697f0
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-11-12 18:45:35 -0500

    adding updated Collection

--- src/main/java/com/collections/java/stack/StackImpl.java
+++ src/main/java/com/collections/java/stack/StackImpl.java
@@ -18,12 +18,45 @@ public class StackImpl {
     public boolean isEmpty() {
         return top == -1;
     }
+
     //to check whether the stack is full
     public boolean isFull() {
         return (size - 1 == top);
     }
-    //peek() - to check the top element - no altering just checking
 
+    //peek() - to check the top element - no altering just checking
+    public int peek() {
+        if (!this.isEmpty()) {
+            return arr[top];
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+    //push() - adding elements into the stack
+    public void push(int element) {
+        if (isFull()) {
+            System.out.println("The stack is full");
+        } else if (!isFull()) {
+            top = top + 1;
+            arr[top] = element;
+            System.out.println("The pushed element is -> " + element);
+        }
+    }
+
+    //pop() - fetch the top value
+    public int pop() {
+        if (!isEmpty()) {
+            int returnTop = top;
+            top--;
+            System.out.println("The popped element is -> " + arr[returnTop]);
+            return arr[returnTop];
+        } else {
+            System.out.println("The stack is empty");
+            return -1;
+        }
+    }
 
     public static void main(String[] args) {
 
