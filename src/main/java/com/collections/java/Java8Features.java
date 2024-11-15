f57e1fe7a8bdf556a1a828434e9b709ec471a57c
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-11-14 19:09:24 -0500

    adding updated Collection and other newer features

--- src/main/java/com/collections/java/Java8Features.java
+++ src/main/java/com/collections/java/Java8Features.java
@@ -1,7 +1,11 @@
 package com.collections.java;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Java8Features {
 
@@ -14,6 +18,17 @@ public class Java8Features {
         }
     }
 
+    //Functional Interfaces
+
+    //Functional interfaces, which are interfaces with a single abstract method.
+    // Functional interfaces are the foundation of lambda expressions and
+    // allow them to be used in collections, streams, and APIs
+
+    @FunctionalInterface
+    interface MyFunctionalInterface {
+        void apply(String str);
+    }
+
     public static void main(String[] args) {
         //Lambda Expressions
 
@@ -24,10 +39,30 @@ public class Java8Features {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         numbers.stream().filter(n -> n % 2 == 0).forEach(nums -> System.out.println(nums));
-        //sout can lasso be used as 'System.out::println'
+        //sout can also be used as 'System.out::println'
 
 
+        //The java.time API (New Date/Time API)
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
+        //Optional Class
+
+        //The Optional class was introduced as a container that may or may not contain a value.
+        // It provides a way to express the absence of a value and helps reduce NullPointerExceptions.
+
+        Optional<String> noMoreExceptions = Optional.ofNullable("no more exceptions");
+        noMoreExceptions.ifPresent(System.out::println);
+
+        //Parallel and Asynchronous Processing
+        //Java 8’s Stream API introduced parallel streams, which enable parallel processing
+        // without requiring developers to manually create threads.
+        // This allows developers to leverage multicore processors more easily.
 
 
+        //This greatly improves performance for certain types of operations,
+        // such as large-scale data processing tasks, with minimal changes to the codebase
+        List<Integer> nums = Arrays.asList(1, 8, 2, 3, 4, 0, 985,1, 8, 2, 3, 4, 0, 985,1, 8, 2, 3, 4, 0, 985,1, 8, 2, 3, 4, 0, 985);
+        nums.parallelStream().forEach(System.out::println); //order will not be preserved
     }
 }
